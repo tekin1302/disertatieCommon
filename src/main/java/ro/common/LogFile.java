725fd72153e3a1a028864301c1ec5ae63faf9c45
tekin.omer <omer-ali.tekin@teamnet.ro>
2015-06-06 14:10:18 +0300

    a

---
+++ src/main/java/ro/common/LogFile.java
@@ -0,0 +1,25 @@
+package ro.common;
+
+/**
+ * Created by tekin.omer on 5/27/2015.
+ */
+public class LogFile {
+    private String fileName;
+    private String content;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
